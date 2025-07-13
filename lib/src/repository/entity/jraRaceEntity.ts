11514da2e197be0555337120ad6f9d9b6b428046
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-07-13 12:09:59 +0900

    レース動画をすぐに見れるように変更

--- lib/src/repository/entity/jraRaceEntity.ts
+++ lib/src/repository/entity/jraRaceEntity.ts
@@ -113,6 +113,7 @@ export class JraRaceEntity implements IRaceEntity<JraRaceEntity> {
     public toGoogleCalendarData(
         updateDate: Date = new Date(),
     ): calendar_v3.Schema$Event {
+        const raceIdForNetkeiba = `${this.raceData.dateTime.getFullYear().toString()}${NetkeibaBabacodeMap[this.raceData.location]}${this.raceData.heldTimes.toXDigits(2)}${this.raceData.heldDayTimes.toXDigits(2)}${this.raceData.number.toXDigits(2)}`;
         return {
             id: generateJraRaceId(
                 this.raceData.dateTime,
@@ -136,7 +137,8 @@ export class JraRaceEntity implements IRaceEntity<JraRaceEntity> {
             description:
                 `距離: ${this.raceData.surfaceType}${this.raceData.distance.toString()}m
                     発走: ${this.raceData.dateTime.getXDigitHours(2)}:${this.raceData.dateTime.getXDigitMinutes(2)}
-                    ${createAnchorTag('レース情報', `https://netkeiba.page.link/?link=https%3A%2F%2Frace.sp.netkeiba.com%2Frace%2Fshutuba.html%3Frace_id%3D${this.raceData.dateTime.getFullYear().toString()}${NetkeibaBabacodeMap[this.raceData.location]}${this.raceData.heldTimes.toXDigits(2)}${this.raceData.heldDayTimes.toXDigits(2)}${this.raceData.number.toXDigits(2)}`)}
+                    ${createAnchorTag('レース情報', `https://netkeiba.page.link/?link=https%3A%2F%2Frace.sp.netkeiba.com%2Frace%2Fshutuba.html%3Frace_id%3D${raceIdForNetkeiba}`)}
+                    ${createAnchorTag('レース動画', `https://netkeiba.page.link/?link=https%3A%2F%2Frace.sp.netkeiba.com%3Fpid%3Drace_movie%26race_id%3D${raceIdForNetkeiba}`)}
                     更新日時: ${format(getJSTDate(updateDate), 'yyyy/MM/dd HH:mm:ss')}
                 `.replace(/\n\s+/g, '\n'),
             extendedProperties: {
