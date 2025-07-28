bd4782a9f8b8fb31ce02cf14e274dc903aa79abf
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-07-28 19:01:44 +0900

    feat: Refactor race data service configuration by removing obsolete file and updating registrations

--- lib/container/service/index.ts
+++ lib/container/service/index.ts
@@ -1,13 +1,13 @@
-import './raceDataServiceConfig';
-
 import { container } from 'tsyringe';
 
 import { PlayerDataService } from '../../src/service/implement/playerDataService';
 import { PublicGamblingCalendarService } from '../../src/service/implement/publicGamblingCalendarService';
 import { PublicGamblingPlaceDataService } from '../../src/service/implement/publicGamblingPlaceDataService';
+import { PublicGamblingRaceDataService } from '../../src/service/implement/publicGamblingRaceDataService';
 import type { ICalendarService } from '../../src/service/interface/ICalendarService';
 import type { IPlaceDataService } from '../../src/service/interface/IPlaceDataService';
 import type { IPlayerDataService } from '../../src/service/interface/IPlayerDataService';
+import type { IRaceDataService } from '../../src/service/interface/IRaceDataService';
 
 container.register<IPlayerDataService>('PlayerDataService', {
     useClass: PlayerDataService,
@@ -17,4 +17,7 @@ container.register<IPlaceDataService>('PublicGamblingPlaceDataService', {
 });
 container.register<ICalendarService>('PublicGamblingCalendarService', {
     useClass: PublicGamblingCalendarService,
+});
+container.register<IRaceDataService>('PublicGamblingRaceDataService', {
+    useClass: PublicGamblingRaceDataService,
 });
