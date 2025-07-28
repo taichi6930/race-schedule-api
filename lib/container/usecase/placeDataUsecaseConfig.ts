59c5cd881367ba6962065ed5e813f02624a1de30
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-07-28 11:14:35 +0900

    古い開催場データユースケースの削除と、PublicGamblingPlaceUseCaseの登録を整理

--- lib/container/usecase/placeDataUsecaseConfig.ts
+++ lib/container/usecase/placeDataUsecaseConfig.ts
@@ -1,43 +1,8 @@
 import { container } from 'tsyringe';
 
-import type { AutoracePlaceData } from '../../src/domain/autoracePlaceData';
-import type { BoatracePlaceData } from '../../src/domain/boatracePlaceData';
-import type { JraPlaceData } from '../../src/domain/jraPlaceData';
-import type { KeirinPlaceData } from '../../src/domain/keirinPlaceData';
-import type { NarPlaceData } from '../../src/domain/narPlaceData';
-import { AutoracePlaceDataUseCase } from '../../src/usecase/implement/autoracePlaceDataUseCase';
-import { BoatracePlaceDataUseCase } from '../../src/usecase/implement/boatracePlaceDataUseCase';
-import { JraPlaceDataUseCase } from '../../src/usecase/implement/jraPlaceDataUseCase';
-import { KeirinPlaceDataUseCase } from '../../src/usecase/implement/keirinPlaceDataUseCase';
-import { NarPlaceDataUseCase } from '../../src/usecase/implement/narPlaceDataUseCase';
 import { PublicGamblingPlaceUseCase } from '../../src/usecase/implement/publicGamblingPlaceUseCase';
-import type { IOldPlaceDataUseCase } from '../../src/usecase/interface/IOldPlaceDataUseCase';
 import type { IPlaceDataUseCase } from '../../src/usecase/interface/IPlaceDataUseCase';
 
-container.register<IOldPlaceDataUseCase<BoatracePlaceData>>(
-    'BoatracePlaceDataUseCase',
-    {
-        useClass: BoatracePlaceDataUseCase,
-    },
-);
-container.register<IOldPlaceDataUseCase<AutoracePlaceData>>(
-    'AutoracePlaceDataUseCase',
-    {
-        useClass: AutoracePlaceDataUseCase,
-    },
-);
-container.register<IOldPlaceDataUseCase<KeirinPlaceData>>(
-    'KeirinPlaceDataUseCase',
-    {
-        useClass: KeirinPlaceDataUseCase,
-    },
-);
-container.register<IOldPlaceDataUseCase<NarPlaceData>>('NarPlaceDataUseCase', {
-    useClass: NarPlaceDataUseCase,
-});
-container.register<IOldPlaceDataUseCase<JraPlaceData>>('JraPlaceDataUseCase', {
-    useClass: JraPlaceDataUseCase,
-});
 container.register<IPlaceDataUseCase>('PublicGamblingPlaceUseCase', {
     useClass: PublicGamblingPlaceUseCase,
 });
