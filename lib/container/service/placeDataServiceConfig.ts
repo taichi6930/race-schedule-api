a801c1c0abe52695971e0efc7bbb844a7ea7ea61
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-07-28 14:12:12 +0900

    古い開催場データサービス関連のインターフェースと実装を削除し、新しいインターフェースに置き換え。関連するテストを更新。

--- lib/container/service/placeDataServiceConfig.ts
+++ lib/container/service/placeDataServiceConfig.ts
@@ -1,53 +1,7 @@
 import { container } from 'tsyringe';
 
-import type { AutoracePlaceEntity } from '../../src/repository/entity/autoracePlaceEntity';
-import type { BoatracePlaceEntity } from '../../src/repository/entity/boatracePlaceEntity';
-import type { JraPlaceEntity } from '../../src/repository/entity/jraPlaceEntity';
-import type { KeirinPlaceEntity } from '../../src/repository/entity/keirinPlaceEntity';
-import type { NarPlaceEntity } from '../../src/repository/entity/narPlaceEntity';
-import { AutoracePlaceDataService } from '../../src/service/implement/autoracePlaceDataService';
-import { BoatracePlaceDataService } from '../../src/service/implement/boatracePlaceDataService';
-import { JraPlaceDataService } from '../../src/service/implement/jraPlaceDataService';
-import { KeirinPlaceDataService } from '../../src/service/implement/keirinPlaceDataService';
-import { NarPlaceDataService } from '../../src/service/implement/narPlaceDataService';
 import { PublicGamblingPlaceDataService } from '../../src/service/implement/publicGamblingPlaceDataService';
-import type { IOldPlaceDataService } from '../../src/service/interface/IOldPlaceDataService';
 import type { IPlaceDataService } from '../../src/service/interface/IPlaceDataService';
-
-container.register<IOldPlaceDataService<JraPlaceEntity>>(
-    'JraPlaceDataService',
-    {
-        useClass: JraPlaceDataService,
-    },
-);
-
-container.register<IOldPlaceDataService<NarPlaceEntity>>(
-    'NarPlaceDataService',
-    {
-        useClass: NarPlaceDataService,
-    },
-);
-
-container.register<IOldPlaceDataService<KeirinPlaceEntity>>(
-    'KeirinPlaceDataService',
-    {
-        useClass: KeirinPlaceDataService,
-    },
-);
-
-container.register<IOldPlaceDataService<AutoracePlaceEntity>>(
-    'AutoracePlaceDataService',
-    {
-        useClass: AutoracePlaceDataService,
-    },
-);
-
-container.register<IOldPlaceDataService<BoatracePlaceEntity>>(
-    'BoatracePlaceDataService',
-    {
-        useClass: BoatracePlaceDataService,
-    },
-);
 
 container.register<IPlaceDataService>('PublicGamblingPlaceDataService', {
     useClass: PublicGamblingPlaceDataService,
