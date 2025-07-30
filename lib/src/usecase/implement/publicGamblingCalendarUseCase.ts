cae602ea24574136d3b09f7040faf42f58996663
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-07-31 01:07:49 +0900

    feat: implement raceType handling in calendar update API

--- lib/src/usecase/implement/publicGamblingCalendarUseCase.ts
+++ lib/src/usecase/implement/publicGamblingCalendarUseCase.ts
@@ -4,7 +4,11 @@ import { inject, injectable } from 'tsyringe';
 
 import { CalendarData } from '../../domain/calendarData';
 import { ICalendarService } from '../../service/interface/ICalendarService';
+import { IRaceDataService } from '../../service/interface/IRaceDataService';
+import { GradeType } from '../../utility/data/base';
+import { DataLocation } from '../../utility/dataType';
 import { Logger } from '../../utility/logger';
+import { RaceType } from '../../utility/sqlite';
 import { IRaceCalendarUseCase } from '../interface/IRaceCalendarUseCase';
 
 /**
@@ -15,6 +19,8 @@ export class PublicGamblingCalendarUseCase implements IRaceCalendarUseCase {
     public constructor(
         @inject('PublicGamblingCalendarService')
         private readonly publicGamblingCalendarService: ICalendarService,
+        @inject('PublicGamblingRaceDataService')
+        private readonly raceDataService: IRaceDataService,
     ) {}
 
     /**
@@ -40,8 +46,85 @@ export class PublicGamblingCalendarUseCase implements IRaceCalendarUseCase {
         return calendarDataList;
     }
 
-    public async updateRacesToCalendar(): Promise<void> {
-        throw new Error('Not implemented');
-        await Promise.resolve();
+    /**
+     * カレンダーの更新を行う
+     * @param startDate
+     * @param finishDate
+     * @param displayGradeList
+     * @param displayGradeList.jra
+     * @param displayGradeList.nar
+     * @param displayGradeList.world
+     * @param displayGradeList.keirin
+     * @param displayGradeList.autorace
+     * @param displayGradeList.boatrace
+     */
+    @Logger
+    public async updateRacesToCalendar(
+        startDate: Date,
+        finishDate: Date,
+        displayGradeList: {
+            jra: GradeType[];
+            nar: GradeType[];
+            world: GradeType[];
+            keirin: GradeType[];
+            autorace: GradeType[];
+            boatrace: GradeType[];
+        },
+    ): Promise<void> {
+        // レース情報を取得する
+        const raceEntityList = await this.raceDataService.fetchRaceEntityList(
+            startDate,
+            finishDate,
+            ['jra'],
+            DataLocation.Storage,
+        );
+
+        // displayGradeListに含まれるレース情報のみを抽出
+        const filteredRaceEntityList = {
+            jra: raceEntityList.jra.filter((raceEntity) =>
+                displayGradeList.jra.includes(raceEntity.raceData.grade),
+            ),
+        };
+        // カレンダーの取得を行う
+        const calendarDataList: CalendarData[] =
+            await this.publicGamblingCalendarService.fetchEvents(
+                startDate,
+                finishDate,
+                ['jra'],
+            );
+
+        // 1. raceEntityListのIDに存在しないcalendarDataListを取得
+        const deleteCalendarDataList = {
+            jra: calendarDataList.filter(
+                (calendarData) =>
+                    !filteredRaceEntityList.jra.some(
+                        (raceEntity) =>
+                            raceEntity.id === calendarData.id &&
+                            calendarData.raceType === RaceType.JRA,
+                    ),
+            ),
+        };
+        await this.publicGamblingCalendarService.deleteEvents({
+            nar: [],
+            jra: deleteCalendarDataList.jra,
+            world: [],
+            keirin: [],
+            boatrace: [],
+            autorace: [],
+        });
+
+        // 2. deleteCalendarDataListのIDに該当しないraceEntityListを取得し、upsertする
+        const upsertRaceEntityList = {
+            jra: filteredRaceEntityList.jra.filter(
+                (raceEntity) =>
+                    !deleteCalendarDataList.jra.some(
+                        (deleteCalendarData) =>
+                            deleteCalendarData.id === raceEntity.id,
+                    ),
+            ),
+        };
+        await this.publicGamblingCalendarService.upsertEvents({
+            jra: upsertRaceEntityList.jra,
+        });
     }
 }
