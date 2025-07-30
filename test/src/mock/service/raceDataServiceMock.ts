35b8f277453bea6d153cd6e0a275a6060e456bc4
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-07-30 09:18:24 +0900

    Remove outdated race data service tests and add new PublicGamblingRaceDataService tests

--- test/src/mock/service/raceDataServiceMock.ts
+++ test/src/mock/service/raceDataServiceMock.ts
@@ -1,21 +1,4 @@
-import type { IPlaceEntity } from '../../../../lib/src/repository/entity/iPlaceEntity';
-import type { IRaceEntity } from '../../../../lib/src/repository/entity/iRaceEntity';
-import type { IOldRaceDataService } from '../../../../lib/src/service/interface/IOldRaceDataService';
 import type { IRaceDataService } from '../../../../lib/src/service/interface/IRaceDataService';
-
-/**
- * RaceDataServiceのモックを作成する
- * @returns モック化されたIRaceDataServiceインターフェースのインスタンス
- */
-export const oldRaceDataServiceMock = <
-    R extends IRaceEntity<R>,
-    P extends IPlaceEntity<P>,
->(): jest.Mocked<IOldRaceDataService<R, P>> => {
-    return {
-        fetchRaceEntityList: jest.fn().mockResolvedValue([] as R[]),
-        updateRaceEntityList: jest.fn().mockResolvedValue([]),
-    };
-};
 
 /**
  * RaceDataServiceのモックを作成する
