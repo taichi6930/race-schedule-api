e729a1c59551b1c4109e8868c2f2563fc05329b3
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-07-31 11:54:37 +0900

    feat: 公営競技カレンダーのユースケースを追加
    - calendarUsecaseConfig.tsを削除し、index.tsにPublicGamblingCalendarUseCaseを登録
    - index.tsにPublicGamblingRaceDataUseCaseを登録

--- lib/container/usecase/calendarUsecaseConfig.ts
+++
@@ -1,8 +0,0 @@
-import { container } from 'tsyringe';
-
-import { PublicGamblingCalendarUseCase } from '../../src/usecase/implement/publicGamblingCalendarUseCase';
-import type { IRaceCalendarUseCase } from '../../src/usecase/interface/IRaceCalendarUseCase';
-
-container.register<IRaceCalendarUseCase>('PublicGamblingCalendarUseCase', {
-    useClass: PublicGamblingCalendarUseCase,
-});
