4293d86bd943bdd25d8eb70a895d72adb35a5d6d
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-07-31 11:46:56 +0900

    feat: 公営競技レースデータユースケースを追加し、関連するメソッドを実装
    - PublicGamblingRaceDataUseCaseクラスを新規作成し、レース開催データを取得するfetchRaceDataListメソッドを実装
    - PublicGamblingPlaceDataServiceおよびPublicGamblingRaceDataServiceからデータを取得し、フィルタリング処理を追加
    - IRaceDataUseCaseインターフェースにfetchRaceDataListメソッドを追加し、検索リストのオプションパラメータを定義
    - updateRaceEntityListメソッドのシグネチャを修正し、非同期処理に変更

--- lib/src/usecase/interface/IRaceDataUseCase.ts
+++ lib/src/usecase/interface/IRaceDataUseCase.ts
@@ -1,5 +1,8 @@
 import type { IRaceData } from '../../domain/iRaceData';
+import type { JraRaceData } from '../../domain/jraRaceData';
 import type { GradeType, RaceCourse, RaceStage } from '../../utility/data/base';
+import type { JraGradeType } from '../../utility/data/jra/jraGradeType';
+import type { JraRaceCourse } from '../../utility/data/jra/jraRaceCourse';
 
 /**
  * レースデータUseCaseのインターフェース
@@ -41,4 +44,46 @@ export interface IOldRaceDataUseCase<
      * @param raceDataList
      */
     upsertRaceDataList: (raceDataList: R[]) => Promise<void>;
+}
+
+export interface IRaceDataUseCase {
+    /**
+     * レース開催データを取得する
+     * @param startDate
+     * @param finishDate
+     * @param searchList
+     */
+    fetchRaceDataList: (
+        startDate: Date,
+        finishDate: Date,
+        // Optional parameters
+        searchList?: {
+            jra: { gradeList?: JraGradeType[]; locationList?: JraRaceCourse[] };
+        },
+    ) => Promise<{
+        jra: JraRaceData[];
+    }>;
+
+    // /**
+    //  * レース開催データを更新する
+    //  * @param startDate
+    //  * @param finishDate
+    //  * @param searchList
+    //  */
+    // updateRaceEntityList: (
+    //     startDate: Date,
+    //     finishDate: Date,
+    //     // Optional parameters
+    //     searchList?: {
+    //         jra: { gradeList?: JraGradeType[]; locationList?: JraRaceCourse[] };
+    //     },
+    // ) => Promise<void>;
+
+    // /**
+    //  * レース開催データを更新する
+    //  * @param raceDataList
+    //  */
+    // upsertRaceDataList: (raceDataList: {
+    //     jra: { raceDataList: JraRaceData[] };
+    // }) => Promise<void>;
 }
