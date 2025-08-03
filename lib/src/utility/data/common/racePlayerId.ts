b01dd8ddca96ae3b46615cc301d35d82ca7695b7
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-08-03 19:13:26 +0900

    feat: RacePlayerIdのバリデーションロジックを統一
    - BoatraceRacePlayerIdSchemaのバリデーションをRacePlayerIdSchemaに統合し、raceTypeに応じた動的なバリデーションを実装
    - AutoraceRacePlayerIdSchemaとKeirinRacePlayerIdSchemaもRacePlayerIdSchemaを利用するように変更
    - 各レースタイプに対するバリデーションの一貫性を向上

--- lib/src/utility/data/common/racePlayerId.ts
+++ lib/src/utility/data/common/racePlayerId.ts
@@ -3,49 +3,55 @@ import { z } from 'zod';
 import { RaceType } from '../../raceType';
 import { validatePositionNumber } from './positionNumber';
 import { validateRaceNumber } from './raceNumber';
+
 /**
  * BoatraceRacePlayerIdのzod型定義
  * boatrace + 8桁の数字（開催日） + 2桁の数字（開催場所）+ 2桁の数字（レース番号）+ 2桁の数字（枠番）
+ * @param raceType
  */
-const BoatraceRacePlayerIdSchema = z
-    .string()
-    .refine((value) => {
-        return value.startsWith('boatrace');
-    }, 'boatraceから始まる必要があります')
-    // boatraceの後に8桁の数字（開催日） + 2桁の数字（開催場所）+ 2桁の数字（レース番号）+ 2桁の数字（枠番）
-    .refine((value) => {
-        return /^boatrace\d{14}$/.test(value);
-    }, 'BoatraceRacePlayerIdの形式ではありません')
-    // レース番号は1~12の範囲
-    .refine((value) => {
-        const raceNumber = Number.parseInt(value.slice(-4, -2));
-        try {
-            validateRaceNumber(raceNumber);
-            return true;
-        } catch {
-            return false;
-        }
-    }, 'レース番号は1~12の範囲である必要があります')
-    // 枠番は1~8の範囲
-    .refine((value) => {
-        const positionNumber = Number.parseInt(value.slice(-2));
-        try {
-            validatePositionNumber(RaceType.BOATRACE, positionNumber);
-            return true;
-        } catch {
-            return false;
-        }
-    }, '枠番は1~6の範囲である必要があります');
+const RacePlayerIdSchema = (raceType: RaceType): z.ZodString => {
+    const lowerCaseRaceType = raceType.toLowerCase();
+    return (
+        z
+            .string()
+            .refine((value) => {
+                return value.startsWith(lowerCaseRaceType);
+            }, `${lowerCaseRaceType}から始まる必要があります`)
+            // raceTypeの後に8桁の数字（開催日） + 2桁の数字（開催場所）+ 2桁の数字（レース番号）+ 2桁の数字（枠番）
+            .refine((value) => {
+                return new RegExp(`^${lowerCaseRaceType}\\d{14}$`).test(value);
+            }, `${lowerCaseRaceType}RacePlayerIdの形式ではありません`)
+            // レース番号は1~12の範囲
+            .refine((value) => {
+                const raceNumber = Number.parseInt(value.slice(-4, -2));
+                try {
+                    validateRaceNumber(raceNumber);
+                    return true;
+                } catch {
+                    return false;
+                }
+            }, 'レース番号は1~12の範囲である必要があります')
+            // 枠番はRaceTypeに応じた範囲
+            .refine((value) => {
+                const positionNumber = Number.parseInt(value.slice(-2));
+                try {
+                    validatePositionNumber(raceType, positionNumber);
+                    return true;
+                } catch {
+                    return false;
+                }
+            }, '枠番が不正です')
+    );
+};
+/**
+ * BoatraceRacePlayerIdのzod型定義
+ */
+const BoatraceRacePlayerIdSchema = RacePlayerIdSchema(RaceType.BOATRACE);
 
 /**
  * BoatraceRacePlayerIdの型定義
  */
 export type BoatraceRacePlayerId = z.infer<typeof BoatraceRacePlayerIdSchema>;
-
-/**
- * RacePlayerIdのzod型定義
- */
-export const UnionRacePlayerIdSchema = z.union([BoatraceRacePlayerIdSchema]);
 
 /**
  * RacePlayerIdのzod型定義
@@ -88,35 +94,7 @@ export const validateRacePlayerId = (
  * AutoraceRacePlayerIdのzod型定義
  * autorace + 8桁の数字（開催日） + 2桁の数字（開催場所）+ 2桁の数字（レース番号）+ 2桁の数字（枠番）
  */
-const AutoraceRacePlayerIdSchema = z
-    .string()
-    .refine((value) => {
-        return value.startsWith('autorace');
-    }, 'autoraceから始まる必要があります')
-    // autoraceの後に8桁の数字（開催日） + 2桁の数字（開催場所）+ 2桁の数字（レース番号）+ 2桁の数字（枠番）
-    .refine((value) => {
-        return /^autorace\d{14}$/.test(value);
-    }, 'AutoraceRacePlayerIdの形式ではありません')
-    // レース番号は1~12の範囲
-    .refine((value) => {
-        const raceNumber = Number.parseInt(value.slice(-4, -2));
-        try {
-            validateRaceNumber(raceNumber);
-            return true;
-        } catch {
-            return false;
-        }
-    }, 'レース番号は1~12の範囲である必要があります')
-    // 枠番は1~8の範囲
-    .refine((value) => {
-        const positionNumber = Number.parseInt(value.slice(-2));
-        try {
-            validatePositionNumber(RaceType.AUTORACE, positionNumber);
-            return true;
-        } catch {
-            return false;
-        }
-    }, '枠番は1~8の範囲である必要があります');
+const AutoraceRacePlayerIdSchema = RacePlayerIdSchema(RaceType.AUTORACE);
 /**
  * AutoraceRacePlayerIdの型定義
  */
@@ -127,47 +105,19 @@ export type AutoraceRacePlayerId = z.infer<typeof AutoraceRacePlayerIdSchema>;
  * KeirinRacePlayerIdのzod型定義
  * keirin + 8桁の数字（開催日） + 2桁の数字（開催場所）+ 2桁の数字（レース番号）+ 2桁の数字（枠番）
  */
-const KeirinRacePlayerIdSchema = z
-    .string()
-    .refine((value) => value.startsWith('keirin'), {
-        message: `keirinから始まる必要があります`,
-    })
-    // keirinの後に8桁の数字（開催日） + 2桁の数字（開催場所）+ 2桁の数字（レース番号）+ 2桁の数字（枠番）
-    .refine((value) => /^keirin\d{14}$/.test(value), {
-        message: `KeirinRacePlayerIdの形式ではありません`,
-    })
-    // レース番号は1~12の範囲
-    .refine(
-        (value) => {
-            const raceNumber = Number.parseInt(value.slice(-4, -2));
-            try {
-                validateRaceNumber(raceNumber);
-                return true;
-            } catch {
-                return false;
-            }
-        },
-        {
-            message: `レース番号は1~12の範囲である必要があります`,
-        },
-    )
-    // 枠番は1~9の範囲
-    .refine(
-        (value) => {
-            const positionNumber = Number.parseInt(value.slice(-2));
-            try {
-                validatePositionNumber(RaceType.KEIRIN, positionNumber);
-                return true;
-            } catch {
-                return false;
-            }
-        },
-        {
-            message: `枠番は1~9の範囲である必要があります`,
-        },
-    );
+const KeirinRacePlayerIdSchema = RacePlayerIdSchema(RaceType.KEIRIN);
+
 /**
  * KeirinRacePlayerIdの型定義
  */
 
 export type KeirinRacePlayerId = z.infer<typeof KeirinRacePlayerIdSchema>;
+
+/**
+ * RacePlayerIdのzod型定義
+ */
+export const UnionRacePlayerIdSchema = z.union([
+    KeirinRacePlayerIdSchema,
+    AutoraceRacePlayerIdSchema,
+    BoatraceRacePlayerIdSchema,
+]);
