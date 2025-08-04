95dbeb8920a8cd0b3cbffe0d8e74ac9a0327cdbd
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-08-04 21:45:44 +0900

    feat: PlaceGradeRecordクラスを追加
    - レース開催のグレード場所データを管理するためのPlaceGradeRecordクラスを新規作成
    - コンストラクタとインスタンス生成メソッドを実装
    - データのコピー機能を追加

---
+++ lib/src/gateway/record/placeGradeRecord.ts
@@ -0,0 +1,71 @@
+import {
+    type GradeType,
+    validateGradeType,
+} from '../../utility/data/common/gradeType';
+import type { PlaceId } from '../../utility/data/common/placeId';
+import { validatePlaceId } from '../../utility/data/common/placeId';
+import { createErrorMessage } from '../../utility/error';
+import type { RaceType } from '../../utility/raceType';
+import { type UpdateDate, validateUpdateDate } from '../../utility/updateDate';
+import type { IRecord } from './iRecord';
+
+/**
+ * Repository層のRecord レース開催のグレード場所データ
+ */
+export class PlaceGradeRecord implements IRecord<PlaceGradeRecord> {
+    /**
+     * コンストラクタ
+     * @param id - ID
+     * @param raceType - レース種別
+     * @param grade - グレード
+     * @param updateDate - 更新日時
+     * @remarks
+     * レース開催場所データを生成する
+     */
+    private constructor(
+        public readonly id: PlaceId,
+        public readonly raceType: RaceType,
+        public readonly grade: GradeType,
+        public readonly updateDate: UpdateDate,
+    ) {}
+
+    /**
+     * インスタンス生成メソッド
+     * @param id - ID
+     * @param raceType
+     * @param grade - グレード
+     * @param updateDate - 更新日時
+     */
+    public static create(
+        id: string,
+        raceType: RaceType,
+        grade: string,
+        updateDate: Date,
+    ): PlaceGradeRecord {
+        try {
+            return new PlaceGradeRecord(
+                validatePlaceId(raceType, id),
+                raceType,
+                validateGradeType(raceType, grade),
+                validateUpdateDate(updateDate),
+            );
+        } catch (error) {
+            throw new Error(
+                createErrorMessage('PlaceGradeRecord create error', error),
+            );
+        }
+    }
+
+    /**
+     * データのコピー
+     * @param partial
+     */
+    public copy(partial: Partial<PlaceGradeRecord> = {}): PlaceGradeRecord {
+        return PlaceGradeRecord.create(
+            partial.id ?? this.id,
+            partial.raceType ?? this.raceType,
+            partial.grade ?? this.grade,
+            partial.updateDate ?? this.updateDate,
+        );
+    }
+}
