b309fddd8d8e0e5b93a5bed2ebb06171fd6bc795
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-08-10 22:43:05 +0900

    feat: mockCalendarRepositoryの引数を更新
    mockCalendarRepositoryでraceTypeとsearchFilterを受け取るための引数を追加しました。

--- test/unittest/src/mock/repository/mockCalendarRepository.ts
+++ test/unittest/src/mock/repository/mockCalendarRepository.ts
@@ -2,7 +2,6 @@ import type { ICalendarRepository } from '../../../../../lib/src/repository/inte
 
 export const mockCalendarRepository = (): jest.Mocked<ICalendarRepository> => {
     return {
-        // raceTypeとsearchFilterを受け取る必要があるため、引数を追加
         getEvents: jest.fn(),
         upsertEvents: jest.fn(),
         deleteEvents: jest.fn(),
