764e5b82448ca14919e141ac096c3a4c8123f6b2
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-08-14 13:50:56 +0900

    refactor: グレードタイプのテストを簡素化し、不要な変数を削除
    - 不要な変数や説明を削除し、正常系および異常系のテストを簡素化しました。
    - 異常系テストのエラーメッセージを動的に生成するように変更しました。

--- test/unittest/src/utility/data/common/gradeType.test.ts
+++ test/unittest/src/utility/data/common/gradeType.test.ts
@@ -5,64 +5,40 @@ import { RaceType } from '../../../../../../lib/src/utility/raceType';
  * RaceCourseのテスト
  */
 describe('GradeType', () => {
-    for (const {
-        descriptions,
-        okPattern,
-        ngPattern,
-        raceType,
-        falseDescriptions,
-    } of [
+    for (const { okPattern, raceType } of [
         {
-            descriptions: '中央競馬グレード',
             okPattern: 'GⅠ',
-            ngPattern: 'G2',
             raceType: RaceType.JRA,
-            falseDescriptions: 'JRAのグレードではありません',
         },
         {
-            descriptions: '地方競馬のグレード',
             okPattern: 'GⅠ',
-            ngPattern: 'G2',
             raceType: RaceType.NAR,
-            falseDescriptions: 'NARのグレードではありません',
         },
         {
-            descriptions: '海外競馬のグレード',
             okPattern: 'GⅠ',
-            ngPattern: 'G2',
             raceType: RaceType.WORLD,
-            falseDescriptions: 'WORLDのグレードではありません',
         },
         {
-            descriptions: '競輪のグレード',
             okPattern: 'GⅠ',
-            ngPattern: 'G2',
             raceType: RaceType.KEIRIN,
-            falseDescriptions: 'KEIRINのグレードではありません',
         },
         {
-            descriptions: 'オートレースのグレード',
             okPattern: 'SG',
-            ngPattern: 'G2',
             raceType: RaceType.AUTORACE,
-            falseDescriptions: 'AUTORACEのグレードではありません',
         },
         {
-            descriptions: 'ボートレースのグレード',
             okPattern: 'SG',
-            ngPattern: 'G2',
             raceType: RaceType.BOATRACE,
-            falseDescriptions: 'BOATRACEのグレードではありません',
         },
     ]) {
-        it(`正常系: ${raceType}のグレードが正常な場合 - ${descriptions}`, () => {
+        it(`正常系: ${raceType}のグレードが正常な場合`, () => {
             const result = validateGradeType(raceType, okPattern);
             expect(result).toBe(okPattern);
         });
 
-        it(`異常系: ${raceType}のグレードが異常な場合 - ${falseDescriptions}`, () => {
-            expect(() => validateGradeType(raceType, ngPattern)).toThrow(
-                falseDescriptions,
+        it(`異常系: ${raceType}のグレードが異常な場合`, () => {
+            expect(() => validateGradeType(raceType, '不正なグレード')).toThrow(
+                `${raceType}のグレードではありません`,
             );
         });
     }
