a5bf01c7553412b0ff0eede642fe43bce196ad8d
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-08-14 13:49:00 +0900

    refactor: 不要な変数を削除し、異常系テストの引数を簡素化
    - テストケースからngCourseを削除し、異常系テストで使用する引数を固定値に変更しました。

--- test/unittest/src/utility/data/common/raceCourse.test.ts
+++ test/unittest/src/utility/data/common/raceCourse.test.ts
@@ -5,35 +5,29 @@ import { RaceType } from '../../../../../../lib/src/utility/raceType';
  * RaceCourseのテスト
  */
 describe('RaceCourse', () => {
-    for (const { okCourse, ngCourse, raceType } of [
+    for (const { okCourse, raceType } of [
         {
             okCourse: '東京',
-            ngCourse: '大井',
             raceType: RaceType.JRA,
         },
         {
             okCourse: '大井',
-            ngCourse: '東京',
             raceType: RaceType.NAR,
         },
         {
             okCourse: 'ロンシャン',
-            ngCourse: '東京',
             raceType: RaceType.WORLD,
         },
         {
             okCourse: '平塚',
-            ngCourse: '東京',
             raceType: RaceType.KEIRIN,
         },
         {
             okCourse: '川口',
-            ngCourse: '東京',
             raceType: RaceType.AUTORACE,
         },
         {
             okCourse: '浜名湖',
-            ngCourse: '東京',
             raceType: RaceType.BOATRACE,
         },
     ]) {
@@ -43,7 +37,7 @@ describe('RaceCourse', () => {
         });
 
         it(`異常系: ${raceType}の競馬場が異常な場合`, () => {
-            expect(() => validateRaceCourse(raceType, ngCourse)).toThrow(
+            expect(() => validateRaceCourse(raceType, '不正な開催場')).toThrow(
                 `${raceType}の開催場ではありません`,
             );
         });
