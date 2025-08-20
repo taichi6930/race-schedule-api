4c70fb5feb0a2e0cb1825d5076a45842037741c2
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-08-20 19:14:36 +0900

    refactor: 不要なテストファイルを削除し、テストケースを整理
    - jraRaceRepositoryFromHtmlImpl.test.tsを削除
    - narRaceRepositoryFromHtmlImpl.test.tsを削除
    - mechanicalRacingRaceRepositoryFromHtmlImpl.test.tsにJraRaceRepositoryFromHtmlImplとNarRaceRepositoryFromHtmlImplのテストケースを追加

--- test/unittest/src/repository/implement/mechanicalRacingRaceRepositoryFromHtmlImpl.test.ts
+++ test/unittest/src/repository/implement/mechanicalRacingRaceRepositoryFromHtmlImpl.test.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import { container } from 'tsyringe';
 
+import { HeldDayData } from '../../../../../lib/src/domain/heldDayData';
 import { PlaceData } from '../../../../../lib/src/domain/placeData';
 import type { IRaceDataHtmlGateway } from '../../../../../lib/src/gateway/interface/iRaceDataHtmlGateway';
 import { MockRaceDataHtmlGateway } from '../../../../../lib/src/gateway/mock/mockRaceDataHtmlGateway';
@@ -10,7 +11,9 @@ import type { RaceEntity } from '../../../../../lib/src/repository/entity/raceEn
 import { SearchRaceFilterEntity } from '../../../../../lib/src/repository/entity/searchRaceFilterEntity';
 import { AutoraceRaceRepositoryFromHtmlImpl } from '../../../../../lib/src/repository/implement/autoraceRaceRepositoryFromHtmlImpl';
 import { BoatraceRaceRepositoryFromHtmlImpl } from '../../../../../lib/src/repository/implement/boatraceRaceRepositoryFromHtmlImpl';
+import { JraRaceRepositoryFromHtmlImpl } from '../../../../../lib/src/repository/implement/jraRaceRepositoryFromHtmlImpl';
 import { KeirinRaceRepositoryFromHtmlImpl } from '../../../../../lib/src/repository/implement/keirinRaceRepositoryFromHtmlImpl';
+import { NarRaceRepositoryFromHtmlImpl } from '../../../../../lib/src/repository/implement/narRaceRepositoryFromHtmlImpl';
 import type { IRaceRepository } from '../../../../../lib/src/repository/interface/IRaceRepository';
 import { getJSTDate } from '../../../../../lib/src/utility/date';
 import { allowedEnvs } from '../../../../../lib/src/utility/env';
@@ -20,12 +23,49 @@ import { SkipEnv } from '../../../../utility/testDecorators';
 // テーブル駆動型テスト
 const testCases = [
     {
+        name: 'JraRaceRepositoryFromHtmlImpl',
+        repositoryClass: JraRaceRepositoryFromHtmlImpl,
+        raceType: RaceType.JRA,
+        startDate: new Date('2024-05-26'),
+        endDate: new Date('2024-05-26'),
+        placeName: '東京',
+        heldDayData: HeldDayData.create(1, 1),
+        grade: undefined,
+        placeDate: new Date('2024-05-26'),
+        expectedLength: 24,
+    },
+    {
+        name: 'NarRaceRepositoryFromHtmlImpl',
+        repositoryClass: NarRaceRepositoryFromHtmlImpl,
+        raceType: RaceType.NAR,
+        startDate: new Date('2024-10-02'),
+        endDate: new Date('2024-10-02'),
+        placeName: '大井',
+        heldDayData: undefined,
+        grade: undefined,
+        placeDate: new Date('2024-10-02'),
+        expectedLength: 12,
+    },
+    {
+        name: 'NarRaceRepositoryFromHtmlImpl',
+        repositoryClass: NarRaceRepositoryFromHtmlImpl,
+        raceType: RaceType.NAR,
+        startDate: new Date('2023-10-08'),
+        endDate: new Date('2023-10-08'),
+        placeName: '盛岡',
+        heldDayData: undefined,
+        grade: undefined,
+        placeDate: new Date('2023-10-08'),
+        expectedLength: 12,
+    },
+    {
         name: 'KeirinRaceRepositoryFromHtmlImpl',
         repositoryClass: KeirinRaceRepositoryFromHtmlImpl,
         raceType: RaceType.KEIRIN,
         startDate: new Date('2024-10-20'),
         endDate: new Date('2024-10-20'),
         placeName: '弥彦',
+        heldDayData: undefined,
         grade: 'GⅠ',
         placeDate: new Date('2024-10-20'),
         expectedLength: 12,
@@ -37,6 +77,7 @@ const testCases = [
         startDate: new Date('2024-11-01'),
         endDate: new Date('2024-11-30'),
         placeName: '川口',
+        heldDayData: undefined,
         grade: 'SG',
         placeDate: new Date('2024-11-04'),
         expectedLength: 12,
@@ -48,6 +89,7 @@ const testCases = [
         startDate: new Date('2024-11-01'),
         endDate: new Date('2024-11-30'),
         placeName: '下関',
+        heldDayData: undefined,
         grade: 'SG',
         placeDate: new Date('2024-11-24'),
         expectedLength: 1,
@@ -61,6 +103,7 @@ for (const {
     startDate,
     endDate,
     placeName,
+    heldDayData,
     grade,
     placeDate,
     expectedLength,
@@ -102,7 +145,7 @@ for (const {
                                         placeDate,
                                         placeName,
                                     ),
-                                    undefined,
+                                    heldDayData,
                                     grade,
                                     getJSTDate(new Date()),
                                 ),
