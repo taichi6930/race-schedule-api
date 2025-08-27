deb9c86a8565ddfe2b37b0733eebf40037bac8f2
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-08-27 16:33:30 +0900

    refactor: mockRaceRepositoryのfetchRaceEntityListメソッドの実装を修正し、searchFilterからraceTypeを直接使用するように変更。raceService.test.tsから不要なsetup関数を削除し、テストの簡素化を図った。

--- test/unittest/src/mock/repository/mockRaceRepository.ts
+++ test/unittest/src/mock/repository/mockRaceRepository.ts
@@ -1,8 +1,16 @@
+import type { SearchPlaceFilterEntity } from '../../../../../lib/src/repository/entity/searchPlaceFilterEntity';
 import type { IRaceRepository } from '../../../../../lib/src/repository/interface/IRaceRepository';
+import { baseRaceEntityList } from '../common/baseCommonData';
 
 export const mockRaceRepository = (): jest.Mocked<IRaceRepository> => {
     return {
-        fetchRaceEntityList: jest.fn().mockResolvedValue(Promise.resolve([])),
+        fetchRaceEntityList: jest
+            .fn()
+            .mockImplementation(
+                async (searchFilter: SearchPlaceFilterEntity) => {
+                    return baseRaceEntityList(searchFilter.raceType);
+                },
+            ),
         registerRaceEntityList: jest.fn().mockResolvedValue(Promise.resolve()),
     };
 };
