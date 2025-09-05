1e9949499597d7bdf7a4aa84198732a963d7d8de
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-09-05 20:45:37 +0900

    feat: PlayerServiceクラスを追加し、選手データの取得と登録/更新機能を実装

---
+++ src/service/implement/playerService.ts
@@ -0,0 +1,49 @@
+import { inject, injectable } from 'tsyringe';
+
+import { PlayerEntity } from '../../../lib/src/repository/entity/playerEntity';
+import { CommonParameter } from '../../commonParameter';
+import { IPlayerRepository } from '../../repository/interface/IPlayerRepository';
+import { IPlayerService } from '../interface/IPlayerService';
+
+@injectable()
+export class PlayerService implements IPlayerService {
+    public constructor(
+        @inject('PlayerRepository')
+        private readonly repository: IPlayerRepository,
+    ) {}
+
+    public async fetchPlayerEntityList(
+        commonParameter: CommonParameter,
+    ): Promise<PlayerEntity[]> {
+        const dataList =
+            await this.repository.fetchPlayerDataList(commonParameter);
+        // PlayerEntityに変換する
+        return dataList
+            .map((item) => {
+                try {
+                    return PlayerEntity.create(
+                        item.race_type,
+                        item.player_no,
+                        item.player_name,
+                        item.priority,
+                    );
+                } catch (error) {
+                    console.error('Error creating PlayerEntity:', error);
+                    return null;
+                }
+            })
+            .filter((item): item is PlayerEntity => item !== null);
+    }
+
+    // 選手登録/更新（バリデーション＋upsert）
+    public async upsertPlayerEntityList(
+        commonParameter: CommonParameter,
+        entityList: PlayerEntity[],
+    ): Promise<void> {
+        // DB登録/更新
+        await this.repository.upsertPlayerEntityList(
+            commonParameter,
+            entityList,
+        );
+    }
+}
