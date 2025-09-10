8f12baff61c56886a3b5411fa1c2957167dba0e0
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-09-10 23:58:39 +0900

    feat: race_stageテーブルを作成し、レース情報の取得にステージ情報を追加

--- src/repository/implement/raceRepositoryForStorage.ts
+++ src/repository/implement/raceRepositoryForStorage.ts
@@ -15,23 +15,14 @@ export class RaceRepositoryForStorage implements IRaceRepository {
         commonParameter: CommonParameter,
         searchRaceFilter: SearchRaceFilterEntity,
     ): Promise<RaceEntity[]> {
-        const fetchRaceEntityListForJra = await this.fetchRaceEntityListForJra(
+        return this.fetchRaceEntityListForType(
             commonParameter,
             searchRaceFilter,
         );
-        const fetchRaceEntityListForNarAndOverseas =
-            await this.fetchRaceEntityListForNarAndOverseas(
-                commonParameter,
-                searchRaceFilter,
-            );
-        return [
-            ...fetchRaceEntityListForJra,
-            ...fetchRaceEntityListForNarAndOverseas,
-        ];
     }
 
     @Logger
-    private async fetchRaceEntityListForJra(
+    private async fetchRaceEntityListForType(
         commonParameter: CommonParameter,
         searchRaceFilter: SearchRaceFilterEntity,
     ): Promise<RaceEntity[]> {
@@ -79,11 +70,13 @@ export class RaceRepositoryForStorage implements IRaceRepository {
                 race.race_number,
                 held_day.held_times,
                 held_day.held_day_times,
+                race_stage.stage,
                 race.created_at,
                 race.updated_at
             FROM race
             INNER JOIN race_condition ON race.id = race_condition.id
             LEFT JOIN held_day ON race.place_id = held_day.id
+            LEFT JOIN race_stage ON race.id = race_stage.id
             ${whereClause}`,
         )
             .bind(...queryParams)
@@ -97,6 +90,7 @@ export class RaceRepositoryForStorage implements IRaceRepository {
                           Number(row.held_day_times),
                       )
                     : undefined;
+            const stage = row.stage === null ? undefined : row.stage;
             return RaceEntity.create(
                 row.id,
                 row.place_id,
@@ -110,95 +104,8 @@ export class RaceRepositoryForStorage implements IRaceRepository {
                 ),
                 heldDayData,
                 HorseRaceConditionData.create(row.surface_type, row.distance),
-                undefined, // TODO: stageを設定する
-                undefined, // TODO: racePlayerDataListを設定する
-            );
-        });
-    }
-
-    @Logger
-    private async fetchRaceEntityListForNarAndOverseas(
-        commonParameter: CommonParameter,
-        searchRaceFilter: SearchRaceFilterEntity,
-    ): Promise<RaceEntity[]> {
-        const { env } = commonParameter;
-        const { raceTypeList, startDate, finishDate, locationList, gradeList } =
-            searchRaceFilter;
-
-        const startDateFormatted = formatDate(startDate, 'yyyy-MM-dd');
-        const finishDateFormatted = formatDate(finishDate, 'yyyy-MM-dd');
-
-        // raceTypeListの数に応じて動的にWHERE句を生成
-        const raceTypePlaceholders = raceTypeList.map(() => '?').join(', ');
-        let whereClause = `WHERE race.race_type IN (${raceTypePlaceholders}) AND race.date_time >= ? AND race.date_time <= ?`;
-
-        const queryParams: any[] = [];
-        queryParams.push(
-            ...raceTypeList,
-            startDateFormatted,
-            finishDateFormatted,
-        );
-
-        if (locationList.length > 0) {
-            const locationPlaceholders = locationList.map(() => '?').join(', ');
-            whereClause += ` AND place.location_name IN (${locationPlaceholders})`;
-            queryParams.push(...locationList);
-        }
-
-        if (gradeList.length > 0) {
-            const gradePlaceholders = gradeList.map(() => '?').join(', ');
-            whereClause += ` AND race.grade IN (${gradePlaceholders})`;
-            queryParams.push(...gradeList);
-        }
-
-        const { results } = await env.DB.prepare(
-            `
-            SELECT
-                race.id,
-                race.place_id,
-                race.race_type,
-                race.race_name,
-                race.date_time,
-                race.location_name,
-                race_condition.surface_type,
-                race_condition.distance,
-                race.grade,
-                race.race_number,
-                held_day.held_times,
-                held_day.held_day_times,
-                race.created_at,
-                race.updated_at
-            FROM race
-            INNER JOIN race_condition ON race.id = race_condition.id
-            LEFT JOIN held_day ON race.place_id = held_day.id
-            ${whereClause}`,
-        )
-            .bind(...queryParams)
-            .all();
-        return results.map((row: any): RaceEntity => {
-            const dateJST = new Date(new Date(row.date_time));
-            const heldDayData =
-                row.held_times !== null && row.held_day_times !== null
-                    ? HeldDayData.create(
-                          Number(row.held_times),
-                          Number(row.held_day_times),
-                      )
-                    : undefined;
-            return RaceEntity.create(
-                row.id,
-                row.place_id,
-                RaceData.create(
-                    row.race_type,
-                    row.race_name,
-                    dateJST,
-                    row.location_name,
-                    row.grade,
-                    row.race_number,
-                ),
-                heldDayData,
-                HorseRaceConditionData.create(row.surface_type, row.distance),
-                undefined, // TODO: stageを設定する
-                undefined, // TODO: racePlayerDataListを設定する
+                stage,
+                undefined,
             );
         });
     }
