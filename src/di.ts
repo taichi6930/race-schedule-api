bcfb4d9e129539c916173cead3ca6ff15bb6d42b
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-09-11 14:10:15 +0900

    feat: DIコンテナの設定を追加し、ルーティング機能を実装 - di.tsを新規作成し、各種リポジトリとサービスをDIコンテナに登録 - router.tsを新規作成し、エンドポイントごとのハンドラーを実装 - index.tsを修正し、ルーターを使用するように変更

---
+++ src/di.ts
@@ -0,0 +1,105 @@
+import 'reflect-metadata';
+
+import { container } from 'tsyringe';
+
+import { DBGateway } from './gateway/implement/dbGateway';
+import { GoogleCalendarGateway } from './gateway/implement/googleCalendarGateway';
+import { PlaceDataHtmlGateway } from './gateway/implement/placeDataHtmlGateway';
+import { RaceDataHtmlGateway } from './gateway/implement/raceDataHtmlGateway';
+import type { ICalendarGateway } from './gateway/interface/iCalendarGateway';
+import type { IDBGateway } from './gateway/interface/iDbGateway';
+import type { IPlaceDataHtmlGateway } from './gateway/interface/iPlaceDataHtmlGateway';
+import type { IRaceDataHtmlGateway } from './gateway/interface/iRaceDataHtmlGateway';
+import { AutoraceRaceRepositoryFromHtml } from './repository/implement/autoraceRaceRepositoryFromHtml';
+import { BoatraceRaceRepositoryFromHtml } from './repository/implement/boatraceRaceRepositoryFromHtml';
+import { GoogleCalendarRepository } from './repository/implement/googleCalendarRepository';
+import { JraRaceRepositoryFromHtml } from './repository/implement/jraRaceRepositoryFromHtml';
+import { KeirinRaceRepositoryFromHtml } from './repository/implement/keirinRaceRepositoryFromHtml';
+import { NarRaceRepositoryFromHtml } from './repository/implement/narRaceRepositoryFromHtml';
+import { OverseasRaceRepositoryFromHtml } from './repository/implement/overseasRaceRepositoryFromHtml';
+import { PlaceRepositoryFromHtml } from './repository/implement/placeRepositoryFromHtml';
+import { PlaceRepositoryFromStorage } from './repository/implement/placeRepositoryFromStorage';
+import { PlayerRepository } from './repository/implement/playerRepository';
+import { RaceRepositoryFromStorage } from './repository/implement/raceRepositoryFromStorage';
+import type { ICalendarRepository } from './repository/interface/ICalendarRepository';
+import type { IPlaceRepository } from './repository/interface/IPlaceRepository';
+import type { IPlayerRepository } from './repository/interface/IPlayerRepository';
+import type { IRaceRepository } from './repository/interface/IRaceRepository';
+import { CalendarService } from './service/implement/calendarService';
+import { PlaceService } from './service/implement/placeService';
+import { PlayerService } from './service/implement/playerService';
+import { RaceService } from './service/implement/raceService';
+import type { ICalendarService } from './service/interface/ICalendarService';
+import type { IPlaceService } from './service/interface/IPlaceService';
+import type { IPlayerService } from './service/interface/IPlayerService';
+import type { IRaceService } from './service/interface/IRaceService';
+import { CalendarUseCase } from './usecase/implement/calendarUseCase';
+import { PlaceUseCase } from './usecase/implement/placeUsecase';
+import { PlayerUseCase } from './usecase/implement/playerUsecase';
+import { RaceUseCase } from './usecase/implement/raceUsecase';
+import type { ICalendarUseCase } from './usecase/interface/ICalendarUseCase';
+import type { IPlaceUseCase } from './usecase/interface/IPlaceUsecase';
+import type { IPlayerUseCase } from './usecase/interface/IPlayerUsecase';
+import type { IRaceUseCase } from './usecase/interface/IRaceUsecase';
+
+container.register<IPlayerRepository>('PlayerRepository', {
+    useClass: PlayerRepository,
+});
+container.register<IPlayerService>('PlayerService', {
+    useClass: PlayerService,
+});
+container.register<IPlayerUseCase>('PlayerUsecase', {
+    useClass: PlayerUseCase,
+});
+container.register<IDBGateway>('DBGateway', { useClass: DBGateway });
+container.register<IRaceDataHtmlGateway>('RaceDataHtmlGateway', {
+    useClass: RaceDataHtmlGateway,
+});
+container.register<IRaceRepository>('RaceRepositoryFromStorage', {
+    useClass: RaceRepositoryFromStorage,
+});
+container.register<IRaceRepository>('JraRaceRepositoryFromHtml', {
+    useClass: JraRaceRepositoryFromHtml,
+});
+container.register<IRaceRepository>('NarRaceRepositoryFromHtml', {
+    useClass: NarRaceRepositoryFromHtml,
+});
+container.register<IRaceRepository>('OverseasRaceRepositoryFromHtml', {
+    useClass: OverseasRaceRepositoryFromHtml,
+});
+container.register<IRaceRepository>('KeirinRaceRepositoryFromHtml', {
+    useClass: KeirinRaceRepositoryFromHtml,
+});
+container.register<IRaceRepository>('AutoraceRaceRepositoryFromHtml', {
+    useClass: AutoraceRaceRepositoryFromHtml,
+});
+container.register<IRaceRepository>('BoatraceRaceRepositoryFromHtml', {
+    useClass: BoatraceRaceRepositoryFromHtml,
+});
+container.register<IRaceService>('RaceService', { useClass: RaceService });
+container.register<IRaceUseCase>('RaceUsecase', { useClass: RaceUseCase });
+container.register<IPlaceDataHtmlGateway>('PlaceDataHtmlGateway', {
+    useClass: PlaceDataHtmlGateway,
+});
+container.register<IPlaceRepository>('PlaceRepositoryFromStorage', {
+    useClass: PlaceRepositoryFromStorage,
+});
+container.register<IPlaceRepository>('PlaceRepositoryFromHtml', {
+    useClass: PlaceRepositoryFromHtml,
+});
+container.register<IPlaceService>('PlaceService', { useClass: PlaceService });
+container.register<IPlaceUseCase>('PlaceUsecase', { useClass: PlaceUseCase });
+container.register<ICalendarGateway>('GoogleCalendarGateway', {
+    useClass: GoogleCalendarGateway,
+});
+container.register<ICalendarRepository>('CalendarRepository', {
+    useClass: GoogleCalendarRepository,
+});
+container.register<ICalendarService>('CalendarService', {
+    useClass: CalendarService,
+});
+container.register<ICalendarUseCase>('CalendarUsecase', {
+    useClass: CalendarUseCase,
+});
+
+export { container } from 'tsyringe';
