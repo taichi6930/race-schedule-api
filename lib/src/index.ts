80bc0163f4cec9b38c994e714a011186e6221e28
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2025-09-18 12:11:15 +0900

    feat: 不要なコントローラーおよびインターフェースファイルを削除し、リポジトリを整理

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -4,23 +4,12 @@ import '../container'; // DIコンテナの設定をインポート
 import serverlessExpress from '@codegenie/serverless-express';
 import type { Application } from 'express';
 import express from 'express';
-import { container } from 'tsyringe';
-
-import { PublicGamblingControllerFromAWS } from './controller/publicGamblingController';
 
 // Expressアプリケーションの設定
 const app: Application = express();
 
-// DIコンテナからControllerを取得
-const publicGamblingController = container.resolve(
-    PublicGamblingControllerFromAWS,
-);
-
 // Expressの設定
 app.use(express.json());
-
-// ルーティングの設定
-app.use('/api/races/all', publicGamblingController.router);
 
 // health check
 app.get('/health', (_, res) => {
