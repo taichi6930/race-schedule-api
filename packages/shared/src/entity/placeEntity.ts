cd1e591d52594d613ce5a7c95a9dd992f86128f8
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2026-01-11 09:04:20 +0900

    feat: PlaceEntityに新機能・修正を追加

--- packages/shared/src/entity/placeEntity.ts
+++ packages/shared/src/entity/placeEntity.ts
@@ -1,3 +1,5 @@
+import type { HeldDayTimes } from '../types/heldDayTimes';
+import type { HeldTimes } from '../types/heldTimes';
 import type { LocationCode } from '../types/locationCode';
 import type { PlaceId } from '../types/placeId';
 import type { RaceType } from './../types/raceType';
@@ -7,4 +9,10 @@ export interface PlaceEntity {
     raceType: RaceType; // レース種別
     datetime: Date; // 開催日付
     locationCode: LocationCode; // 開催場コード
+    placeHeldDays: PlaceHeldDays | undefined; // 開催回数
+}
+
+export interface PlaceHeldDays {
+    heldTimes: HeldTimes; // 開催回数
+    heldDayTimes: HeldDayTimes; // 開催日数
 }
